ad93d3943edc5e54164025431dd79bb9ed42865a
loretari <loretari@gmail.com>
2023-10-23 13:57:17 +0300

    init: add Banner component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Header from "../Components/Header/Header";
 import ProudProducts from "../Components/ProudProducts/ProudProducts";
+import Banner from "../Components/Banner/Banner";
+import Banner1 from '../Components/Assets/Banner/Banner1.jpg';
+import Banner2 from '../Components/Assets/Banner/Banner2.jpg';
+import BannerReverse from "../Components/Banner/BannerReverse";
 
 
 function Home () {
@@ -8,6 +12,16 @@ function Home () {
         <>
             <Header />
             <ProudProducts />
+            <Banner
+            title= "Cloaks from Wool we love"
+            text = "Discover universally flattering and endlessly versatile styles, perfect for fall."
+            img={Banner1}
+            />
+            <BannerReverse
+            title= "Dresses it is so charming."
+            text= "Discover a fresh twist on power dressing with playful proportions."
+            img={Banner2}
+            />
        </>
     )
 }
