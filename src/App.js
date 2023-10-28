bb53f66613e959049952617ffb85460d18619a63
loretari <loretari@gmail.com>
2023-10-28 10:32:12 +0300

    init: change ProductPage and fix bugs

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import All from "./Components/ShopBy-pages/All";
 import Shoes from "./Components/ShopBy-pages/Shoes";
 import Bags from "./Components/ShopBy-pages/Bags";
 import ClothingPage from "./Pages/ClothingPage";
-import BagsPage from "./Pages/BagsPage";
+// import BagsPage from "./Pages/BagsPage";
 import ShoesPage from "./Pages/ShoesPage";
 import AllPage from "./Pages/AllPage";
 import ProductPage from "./Pages/ProductPage";
